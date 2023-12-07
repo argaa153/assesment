580323a0cfdfec9f70334ce4c318f3dc3022bac7
Daniel <argaa153@gmail.com>
2023-12-07 11:43:05 +0700

    add java stream

--- apps/src/main/java/com/java/app/controller/Controller.java
+++ apps/src/main/java/com/java/app/controller/Controller.java
@@ -1,6 +1,7 @@
 package com.java.app.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,16 +20,22 @@ public class Controller {
 	public String home() {
 		return "Training Java";
 	}
-	
+
 	@RequestMapping(value = "/car")
 	public List<Car> car() {
 		return service.getCar();
 	}
 
 	@RequestMapping(value = "/car", params = "brand")
-	public List<?> car(@RequestParam(name = "brand") String brand) {
+	public List<?> carBrand(@RequestParam(name = "brand") String brand) {
 		return service.getCarByBrand(brand);
 
+	}
+
+	@RequestMapping(value = "/car", params = "prefix")
+	public List<Car> carStream(@RequestParam(name = "prefix") String prefix) {
+		List<Car> res = service.getCar();
+		return res.stream().filter(s -> s.getName().startsWith(prefix)).collect(Collectors.toList());
 	}
 
 }
